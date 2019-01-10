1410b9bed9f68b833517d1e5405ae0dfa31adb77
amper43 <amper43@corpglory.com>
2019-01-10 21:48:04 +0300

    fix according rewiev

--- src/project_schema/index.ts
+++ src/project_schema/index.ts
@@ -2,18 +2,19 @@ import build from './build';
 import src from './src';
 
 import { FolderGenerator, TemplateGenerator } from 'src/generators';
-import { TemplateOptions, PluginType, Language, srcExt } from 'src/template_options';
+import { TemplateOptions, Language } from 'src/template_options';
 
 
 export default new FolderGenerator<TemplateOptions>(context => context.options.id, [
-  new TemplateGenerator(require('./package.json.ejs'),
-                        'package.json',
-                        (ctx) => { ctx['extension'] = srcExt('{ext}')(ctx) }
+  new TemplateGenerator(
+    require('./package.json.ejs'),
+    'package.json',
+    (ctx) => { ctx['isTypeScript'] = ctx.options.language === Language.TypeScript }
   ),
   new TemplateGenerator(require('./.gitignore.ejs'), '.gitignore'),
   new TemplateGenerator(require('./README.md.ejs'), 'README.md'),
   context => {
-    if(context.options.pluginType === PluginType.Panel && context.options.language === Language.TypeScript) {
+    if(context.options.language === Language.TypeScript) {
       return new TemplateGenerator(require('./tsconfig.json.ejs'), 'tsconfig.json');
     }
   },
