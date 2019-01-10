648e04764e4f25f98d56efe30aedc44eba1d50ea
amper43 <amper43@corpglory.com>
2019-01-10 20:51:30 +0300

    add support of typescript for panel plugin

--- src/project_schema/build/index.ts
+++ src/project_schema/build/index.ts
@@ -1,8 +1,12 @@
-import { FolderGenerator, TemplateGenerator } from 'src/generators';
+import { FolderGenerator, TemplateGenerator, GenerationContext } from 'src/generators';
+import { srcExt, TemplateOptions } from 'src/template_options';
 
 
 export default new FolderGenerator('build', [
-  new TemplateGenerator(require('./webpack.base.conf.js.ejs'), 'webpack.base.conf.js'),
+  new TemplateGenerator(require('./webpack.base.conf.js.ejs'),
+  'webpack.base.conf.js',
+  (ctx: GenerationContext<TemplateOptions>) => { ctx['extension'] = srcExt('{ext}')(ctx) }
+  ),
   new TemplateGenerator(require('./webpack.dev.conf.js.ejs'), 'webpack.dev.conf.js'),
   new TemplateGenerator(require('./webpack.prod.conf.js.ejs'), 'webpack.prod.conf.js'),
 ]);
--- src/project_schema/index.ts
+++ src/project_schema/index.ts
@@ -2,12 +2,20 @@ import build from './build';
 import src from './src';
 
 import { FolderGenerator, TemplateGenerator } from 'src/generators';
-import { TemplateOptions } from 'src/template_options';
+import { TemplateOptions, PluginType, Language, srcExt } from 'src/template_options';
 
 
 export default new FolderGenerator<TemplateOptions>(context => context.options.id, [
-  new TemplateGenerator(require('./package.json.ejs'), 'package.json'),
+  new TemplateGenerator(require('./package.json.ejs'),
+                        'package.json',
+                        (ctx) => { ctx['extension'] = srcExt('{ext}')(ctx) }
+  ),
   new TemplateGenerator(require('./.gitignore.ejs'), '.gitignore'),
   new TemplateGenerator(require('./README.md.ejs'), 'README.md'),
+  context => {
+    if(context.options.pluginType === PluginType.Panel && context.options.language === Language.TypeScript) {
+      return new TemplateGenerator(require('./tsconfig.json.ejs'), 'tsconfig.json');
+    }
+  },
   build, src
 ]);
