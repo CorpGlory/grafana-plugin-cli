9675abd4bc50d03aa0ea8a80c526bd984de2654d
amper43 <amper43@corpglory.com>
2019-01-15 14:53:10 +0300

    split panel and datasource css

--- src/project_schema/src/css/index.ts
+++ src/project_schema/src/css/index.ts
@@ -4,10 +4,11 @@ import { TemplateOptions, Style } from 'src/template_options';
 export default new FolderGenerator<TemplateOptions>('css', [
   context => {
     if(context.options.style === Style.CSS) {
+      let pluginType = context.options.pluginType;
       return [
-        new TemplateGenerator(require('./panel.base.css.ejs'), 'panel.base.css'),
-        new TemplateGenerator(require('./panel.light.css.ejs'), 'panel.light.css'),
-        new TemplateGenerator(require('./panel.dark.css.ejs'), 'panel.dark.css')
+        new TemplateGenerator(require(`./${pluginType}.base.css.ejs`), `${pluginType}.base.css`),
+        new TemplateGenerator(require(`./${pluginType}.light.css.ejs`), `${pluginType}.light.css`),
+        new TemplateGenerator(require(`./${pluginType}.dark.css.ejs`), `${pluginType}.dark.css`)
       ];
     } else {
       return [];
